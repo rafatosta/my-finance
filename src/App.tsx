707d281cdddc9f3973b988ba6d973d3435bfbd12
Rafael Tosta <rafa.ecomp@gmail.com>
2023-01-03 11:48:18 -0300

    :construction: Painel principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import CardValue from "./components/CardValue";
+
 function App() {
   return (
-    <div>
-      <div>olá mundo</div>
+    <div className="flex flex-row w-full h-screen justify-center">
+      <div className="m-16 p-8 w-full  rounded-xl bg-gray-100  shadow-2xl">
+        <div className="flex flex-row w-full justify-between">
+          <CardValue title="Despesa" value="200" type="d" />
+          <CardValue title="Receita" value="200" type="r" />
+          <CardValue title="Total" value="200" type="t" />
+        </div>
+      </div>
     </div>
   );
 }
