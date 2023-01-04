19143797f9ff5ee56ff1bbd631703cf59edcc89f
Rafael Tosta <rafa.ecomp@gmail.com>
2023-01-04 20:42:03 -0300

    :recycle: Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,123 +4,30 @@ import Transaction from "./@types/Transaction";
 import { useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
 
+import { useTransaction } from "./hooks/useTransaction";
+
 function App() {
-  const listApi: Transaction[] = [
-    {
-      id: 0,
-      description: "carro",
-      value: 200,
-      type: "despesa",
-      date: "03/01/2023",
-    },
-    {
-      id: 1,
-      description: "salário",
-      value: 1200,
-      type: "receita",
-      date: "03/01/2023",
-    },
-    {
-      id: 3,
-      description: "Alimentação",
-      value: 500,
-      type: "despesa",
-      date: "03/01/2023",
-    },
-    {
-      id: 4,
-      description: "Escola",
-      value: 200,
-      type: "despesa",
-      date: "03/01/2023",
-    },
-  ];
-
-  const [list, setList] = useState<Transaction[]>(listApi);
-
-  const [idLast, setIdLast] = useState<number>(5);
-  const [despesa, setDespesa] = useState<number>(900);
-  const [receita, setReceita] = useState<number>(1200);
-
-  const { register, handleSubmit, reset } = useForm<Transaction>();
-
-  function handleRegistration(data: Transaction) {
-    data.id = idLast;
-    setIdLast(idLast + 1);
-    if (data.type === "receita") {
-      setReceita(receita + data.value);
-    } else {
-      setDespesa(data.value + despesa);
-    }
-    setList([...list, data]);
-    reset();
-  }
-
-  function onDelete(data: Transaction) {
-    list.splice(list.indexOf(data), 1);
-    setList([...list]);
-  }
-
+  const { data, setData, despesa, receita, total } = useTransaction();
   return (
-    <div className="flex flex-col w-full h-screen justify-center gap-y-10 m-5 sm:p-20 lg:m-15 p-10">
-      <div>
-        <h1 className="text-4xl font-bold ">My Finance</h1>
-      </div>
-      <div className="flex flex-row w-full justify-around gap-x-2">
-        <CardValue title="Despesa" value={despesa} type="d" />
-        <CardValue title="Receita" value={receita} type="r" />
-        <CardValue title="Total" value={receita - despesa} type="t" />
-      </div>
-
-      <div>
-        <form
-          className="flex flex-col lg:flex-row bg-white  rounded-xl border shadow p-4 justify-center gap-y-4 sm:gap-x-4"
-          onSubmit={handleSubmit(handleRegistration)}
-        >
-          <input
-            className="p-2 border rounded-sm"
-            type="text"
-            required
-            placeholder="Descrição"
-            {...register("description")}
-          />
-          <input
-            className="p-2 border rounded-sm"
-            type="number"
-            required
-            placeholder="Valor"
-            {...register("value", {
-              valueAsNumber: true,
-            })}
-          />
-          <select className="p-2 border rounded-sm" {...register("type")}>
-            <option value="receita">Receita</option>
-            <option value="despesa">Despesa</option>
-          </select>
-          <input
-            className="p-2 border rounded-sm"
-            type="date"
-            value="2023-01-03"
-            required
-            {...register("date")}
-          />
-          <button
-            className="px-2 border rounded-lg bg-blue-500 hover:bg-blue-700 active:bg-blue-900 w-20 h-10 text-white font-bold"
-            type="submit"
-          >
-            ok
+    <div className="flex flex-col bg-[#1E1E1E] w-full h-screen text-white">
+      <div className="fixed flex flex-row justify-between items-center w-full h-16 px-4 bg-[#242424] text-white text-lg text-sans font-black shadow-2xl">
+        <div>$ Minhas finanças</div>
+        <div>
+          Saldo: R$
+          <span className={total > 0 ? "text-blue-500" : "text-red-500"}>
+            {" "}
+            {total}
+          </span>
+        </div>
+        <div>
+          <button className="bg-green-700 h-max-4 hover:bg-green-800 active:bg-green-900 p-2 rounded-lg">
+            + Nova transação
           </button>
-        </form>
+        </div>
       </div>
-
-      <div className="flex justify-center">
-        <Table
-          data={list}
-          onDelete={(data) => {
-            onDelete(data);
-          }}
-        />
-      </div>
+      
+        <div className="m-8 mt-20 bg-[#242424]">despesa e receita</div>
+      
     </div>
   );
 }
