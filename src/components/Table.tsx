497b7eab18f00196d995d74f632db24bed1d2bda
Rafael Tosta <rafa.ecomp@gmail.com>
2023-01-05 16:06:23 -0300

    Update Table.tsx
    Move o título para o layout principal

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -8,99 +8,90 @@ type TableProps = {
 
 function Table(props: TableProps) {
   return (
-    <div className="rounded-lg bg-[#242424]">
-      <div className="rounded-t mb-0 px-4 py-3 border-0">
-        <div className="flex flex-wrap items-center">
-          <h3 className="font-semibold text-lg text-white">Lançamentos</h3>
-        </div>
-      </div>
-      <div className="block w-full overflow-x-auto">
-        <table className="table-auto items-center w-full bg-transparent border-collapse">
-          <thead>
-            <tr>
-              <CellHeader className="w-2">Tipo</CellHeader>
-              <CellHeader>Desrição</CellHeader>
-              <CellHeader>Valor</CellHeader>
-              <CellHeader className="w-2">Ações</CellHeader>
-            </tr>
-          </thead>
-          <tbody>
-            {props.data?.map((item) => {
-              return (
-                <tr key={item.id}>
-                  <Cell
-                    className={`text-center font-bold ${
-                      item.type === "receita"
-                        ? "text-green-600"
-                        : "text-red-600"
-                    } `}
-                  >
-                    {item.type === "receita" ? (
-                      <div className="flex flex-col items-center">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="w-6 h-4"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
-                          />
-                        </svg>
-
-                        <span>Receita</span>
-                      </div>
-                    ) : (
-                      <div className="flex flex-col items-center">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="w-6 h-4"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75"
-                          />
-                        </svg>
-
-                        <span>Despesa</span>
-                      </div>
-                    )}
-                  </Cell>
-                  <Cell>{item.description}</Cell>
-                  <Cell>R$ {item.value}</Cell>
-                  <Cell className="hover:text-red-700 text-center">
-                    <button onClick={() => props.onDelete(item)}>
+    <div className="block w-full overflow-x-auto">
+      <table className="table-auto items-center w-full bg-transparent border-collapse">
+        <thead>
+          <tr>
+            <CellHeader className="w-2">Tipo</CellHeader>
+            <CellHeader>Desrição</CellHeader>
+            <CellHeader>Valor</CellHeader>
+            <CellHeader className="w-2">Ações</CellHeader>
+          </tr>
+        </thead>
+        <tbody>
+          {props.data?.map((item) => {
+            return (
+              <tr key={item.id}>
+                <Cell
+                  className={`text-center font-bold ${
+                    item.type === "receita" ? "text-green-600" : "text-red-600"
+                  } `}
+                >
+                  {item.type === "receita" ? (
+                    <div className="flex flex-col items-center">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
                         strokeWidth={1.5}
                         stroke="currentColor"
-                        className="w-5 h-5"
+                        className="w-6 h-4"
                       >
                         <path
                           strokeLinecap="round"
                           strokeLinejoin="round"
-                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                          d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
                         />
                       </svg>
-                    </button>
-                  </Cell>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+
+                      <span>Receita</span>
+                    </div>
+                  ) : (
+                    <div className="flex flex-col items-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-4"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75"
+                        />
+                      </svg>
+
+                      <span>Despesa</span>
+                    </div>
+                  )}
+                </Cell>
+                <Cell>{item.description}</Cell>
+                <Cell>R$ {item.value}</Cell>
+                <Cell className="hover:text-red-700 text-center">
+                  <button onClick={() => props.onDelete(item)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-5 h-5"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                      />
+                    </svg>
+                  </button>
+                </Cell>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
