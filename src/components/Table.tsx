5cbf725c0b717ed4c09fed50ef0880f328d4fd73
Rafael Tosta <rafa.ecomp@gmail.com>
2023-01-05 15:08:00 -0300

    :recycle: Table component

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -3,71 +3,114 @@ import tw from "tailwind-styled-components";
 
 type TableProps = {
   data: Transaction[];
-  onDelete: (item: Transaction) => void;
+  onDelete?: (item: Transaction) => void;
 };
 
 function Table(props: TableProps) {
   return (
-    <div className="rounded-xl border shadow-xl py-2 w-full">
-      <table className="table-fixed w-full">
-        <thead>
-          <tr>
-            <CellHeader>#</CellHeader>
-            <CellHeader>Descrição</CellHeader>
-            <CellHeader>Valor</CellHeader>
-            <CellHeader>Tipo</CellHeader>
-            <CellHeader>Data</CellHeader>
-            <CellHeader></CellHeader>
-          </tr>
-        </thead>
-        <tbody>
-          {props.data.map((item) => {
-            return (
-              <tr key={item.id} className="hover:scale-[1.01]">
-                <Cell>{item.id}</Cell>
-                <Cell>{item.description}</Cell>
-                <Cell>{item.value}</Cell>
-                <Cell
-                  className= {
-                    item.type === "receita" ? "text-green-600" : "text-red-600"
-                  }  
-                >
-                  {item.type}
-                </Cell>
-                <Cell>{item.date}</Cell>
-                <Cell className="hover:text-red-700">
-                  <button onClick={() => props.onDelete(item)}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                      />
-                    </svg>
-                  </button>
-                </Cell>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+    <div className="rounded-lg bg-[#242424]">
+      <div className="rounded-t mb-0 px-4 py-3 border-0">
+        <div className="flex flex-wrap items-center">
+          <h3 className="font-semibold text-lg text-white">Lançamentos</h3>
+        </div>
+      </div>
+      <div className="block w-full overflow-x-auto">
+        <table className="table-auto items-center w-full bg-transparent border-collapse">
+          <thead>
+            <tr>
+              <CellHeader className="w-2">Tipo</CellHeader>
+              <CellHeader>Desrição</CellHeader>
+              <CellHeader>Valor</CellHeader>
+              <CellHeader className="w-2">Ações</CellHeader>
+            </tr>
+          </thead>
+          <tbody>
+            {props.data?.map((item) => {
+              return (
+                <tr key={item.id}>
+                  <Cell
+                    className={`text-center font-bold ${
+                      item.type === "receita"
+                        ? "text-green-600"
+                        : "text-red-600"
+                    } `}
+                  >
+                    {item.type === "receita" ? (
+                      <div className="flex flex-col items-center">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-6 h-4"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
+                          />
+                        </svg>
+
+                        <span>Receita</span>
+                      </div>
+                    ) : (
+                      <div className="flex flex-col items-center">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-6 h-4"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75"
+                          />
+                        </svg>
+
+                        <span>Despesa</span>
+                      </div>
+                    )}
+                  </Cell>
+                  <Cell>{item.description}</Cell>
+                  <Cell>R$ {item.value}</Cell>
+                  <Cell className="hover:text-red-700 text-center">
+                    <button onClick={() => props.onDelete(item)}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-5 h-5"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                        />
+                      </svg>
+                    </button>
+                  </Cell>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
 
 const CellHeader = tw.th`
-table-cell px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider align-middle
+px-6 align-middle border border-2 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-[#242424] text-white border-[#1E1E1E]
 `;
 
 const Cell = tw.td`
-table-cell px-5 py-3 border-b border-gray-200 bg-white text-sm w-2/5 align-middle text-center capitalize
+border-t-0 px-6 align-middle border-l-0 border-r-0 text-sm font-bold whitespace-nowrap p-4 capitalize text-left
 `;
 
 export default Table;
